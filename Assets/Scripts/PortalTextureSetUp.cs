0dcaed4ff1aaca3a8a0d90482ce0968747b65ed7
jiawen9201 <waiwen@graduate.utm.my>
2024-05-17 15:21:48 +0800

    Initial commit

---
+++ Assets/Scripts/PortalTextureSetUp.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetUp : MonoBehaviour
+{
+    public Camera cameraA;
+    public Camera cameraB;
+
+    public Material cameraMatA;
+    public Material cameraMatB;
+    
+    void Start()
+    {
+        if (cameraB.targetTexture != null)
+        {
+            cameraB.targetTexture.Release();
+        }
+        cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatB.mainTexture = cameraB.targetTexture;
+
+        if (cameraA.targetTexture != null)
+        {
+            cameraA.targetTexture.Release();
+        }
+        cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatA.mainTexture = cameraA.targetTexture;
+    }
+
+    void Update()
+    {
+        if (cameraB.targetTexture != null)
+        {
+            cameraB.targetTexture.Release();
+        }
+        cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatB.mainTexture = cameraB.targetTexture;
+
+        if (cameraA.targetTexture != null)
+        {
+            cameraA.targetTexture.Release();
+        }
+        cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatA.mainTexture = cameraA.targetTexture;
+    }
+}
